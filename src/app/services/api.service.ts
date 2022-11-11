1d6a57f1a64b610f7c3676a06c9d9932afbb528b
Joel Navarro Rivero <jnr_navarro_rivero@hotmail.com>
2022-11-11 12:31:05 +0000

    changes to swiper

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { shareReplay } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class Api {
-    protected host: string = 'https://2bd3-83-59-214-124.eu.ngrok.io/api'
+    protected host: string = 'http://127.0.0.1:8000/api'
     private static token?: string = null;
 
     constructor(protected http: HttpClient) {
