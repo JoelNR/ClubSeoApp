6e1388d5acff29c22b5650562960186ef3baac12
Joel Navarro Rivero <jnr_navarro_rivero@hotmail.com>
2023-01-05 13:29:20 +0000

    backToTheTop Button, change archer arrows

--- src/app/views/competitions/score-competition/score-competition.page.ts
+++ src/app/views/competitions/score-competition/score-competition.page.ts
@@ -7,21 +7,42 @@ import { CapacitorBase } from 'src/app/lib/CapacitorBase';
   styleUrls: ['./score-competition.page.scss'],
 })
 export class ScoreCompetitionPage extends CapacitorBase implements OnInit {
-  archers: string[] = ['Jose Antonio Rodríguez', 'Joel Navarro']
-  target: string[] = ['1A','1B']
+  archers: string[] = ['Jose Antonio Rodríguez', 'Joel Navarro','Joele Navarro','Joely Navarro']
+  target: string[] = ['1A','1B','1C','1D']
   title: string = 'Trofeo caramelo'
-  total: number[] = [0,0]
+  total: number[] = [0,0,0,0]
   index: number = 0
+  selectedArcher: string
 
   constructor() {
     super()
    }
 
   ngOnInit() {
+    this.selectedArcher = this.archers[0]
   }
 
   changeArcher(event: any){
-    this.index = this.archers.findIndex(archer => archer == event)
+    this.index = this.archers.findIndex(archer => archer == event.target.value)
+    this.selectedArcher =  this.archers[this.index]
+  }
+
+  changeIndexRight(){
+    if(this.index >=  this.archers.length-1){
+      this.index = 0
+    } else {
+      this.index++
+    }
+    this.selectedArcher =  this.archers[this.index]
+  }
+
+  changeIndexLeft(){
+    if(this.index <=  0){
+      this.index = this.archers.length-1
+    } else {
+      this.index--
+    }
+    this.selectedArcher =  this.archers[this.index]
   }
 
 }
