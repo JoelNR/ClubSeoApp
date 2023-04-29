7740f8a0bde339f8d82cdd2a2bf9174094e68b2f
Joel Navarro Rivero <jnr_navarro_rivero@hotmail.com>
2023-04-29 09:49:05 +0100

    today competition

--- src/app/views/competitions/competitions.page.ts
+++ src/app/views/competitions/competitions.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from 'console';
 import * as dayjs from 'dayjs';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { CapacitorBase } from 'src/app/lib/CapacitorBase';
@@ -13,9 +14,11 @@ import { CompetitionService } from 'src/app/services/competition.service';
 export class CompetitionsPage extends CapacitorBase implements OnInit {
   competitionArray: CompetitionModel[]
   pastCompetitionArray: CompetitionModel[]
+  todayCompetitionArray: CompetitionModel[] = []
   searchKeyword: string = ""
   results: any = []
   pastResults: any = []
+  todayResults: any = []
 
   constructor(private competitionService: CompetitionService,
     private ngxService: NgxUiLoaderService) { 
@@ -30,11 +33,15 @@ export class CompetitionsPage extends CapacitorBase implements OnInit {
     this.ngxService.startLoader("loader-competition");
     this.competitionService.competition().subscribe(res => {
       this.competitionArray = res.data.competitions;
-      let pastIndex = this.competitionArray.findIndex(competition =>  dayjs(competition.date).isBefore(dayjs()))
+      let pastIndex = this.competitionArray.findIndex(competition =>  dayjs(competition.date).isBefore(dayjs()))   
       this.pastCompetitionArray = this.competitionArray.splice(pastIndex)
+      if (dayjs().isSame(dayjs(this.pastCompetitionArray[0].date),'day')){
+        this.todayCompetitionArray.push(this.pastCompetitionArray.shift()) 
+      }
       this.competitionArray = this.competitionArray.splice(0,pastIndex)
       this.results = this.competitionArray
       this.pastResults = this.pastCompetitionArray
+      this.todayResults = this.todayCompetitionArray
       this.ngxService.stopLoader('loader-competition');
     });
   }
@@ -43,11 +50,13 @@ export class CompetitionsPage extends CapacitorBase implements OnInit {
     if (this.searchKeyword === "") {
       this.results = [...this.competitionArray]
       this.pastResults = [...this.pastCompetitionArray]
+      this.todayResults = [... this.todayCompetitionArray]
       return
     }
 
     this.results = this.competitionArray.filter(result => result.title.toLowerCase().includes(this.searchKeyword.toLowerCase()))
     this.pastResults = this.pastCompetitionArray.filter(result => result.title.toLowerCase().includes(this.searchKeyword.toLowerCase()))
+    this.todayResults = this.todayCompetitionArray.filter(result => result.title.toLowerCase().includes(this.searchKeyword.toLowerCase()))
   }
 
   handleRefresh(event) {
