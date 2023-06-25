de4d5cef9512b527ee36828f4f144ffc2e0aae5d
Joel Navarro Rivero <jnr_navarro_rivero@hotmail.com>
2023-06-25 21:47:47 +0100

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { shareReplay } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class Api {
-    protected host: string = 'https://3472-83-59-218-172.ngrok-free.app/api'
+    protected host: string = 'http://127.0.0.1:8000/api'
     private static token?: string = null;
 
     constructor(protected http: HttpClient) {
