3d8f0698c5627732a567eda6eaba902ec3aa3170
Joel Navarro Rivero <72139299+JoelNR@users.noreply.github.com>
2024-05-05 18:49:59 +0100

    muchos cambios

--- src/app/views/competitions/competitions-routing.module.ts
+++ src/app/views/competitions/competitions-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
     path: 'detalles/:id',
     loadChildren: () => import('./competition-detail/competition-detail.module').then( m => m.CompetitionDetailPageModule)
   },
-  {
-    path: 'puntuar/:id',
-    loadChildren: () => import('./score-competition/score-competition.module').then( m => m.ScoreCompetitionPageModule)
-  }
 
 ];
 
