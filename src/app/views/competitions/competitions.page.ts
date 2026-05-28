40e5246830b305b5734a206a9fa06eb017c574fb
Héctor Macías <hectormaciasc@gmail.com>
2026-05-28 11:10:38 +0100

    feat: redesign competitions and initiation pages with premium UI components and improved layout styling

--- src/app/views/competitions/competitions.page.ts
+++ src/app/views/competitions/competitions.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import * as dayjs from 'dayjs';
 import { CapacitorBase } from 'src/app/lib/CapacitorBase';
 import { CompetitionModel } from 'src/app/models/competition';
 import { CompetitionService } from 'src/app/services/competition.service';
+import { IonSearchbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-competitions',
@@ -10,6 +11,8 @@ import { CompetitionService } from 'src/app/services/competition.service';
   styleUrls: ['./competitions.page.scss'],
 })
 export class CompetitionsPage extends CapacitorBase implements OnInit {
+  @ViewChild('searchbar', { static: false }) searchbar!: IonSearchbar;
+
   competitionArray: CompetitionModel[] = []
   pastCompetitionArray: CompetitionModel[] = []
   searchKeyword: string = ""
@@ -22,6 +25,24 @@ export class CompetitionsPage extends CapacitorBase implements OnInit {
 
   ngOnInit() {
     this.getCompetitionData();
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    // Focus searchbar when '/' is pressed, unless user is typing in an input/textarea
+    const activeElement = document.activeElement;
+    const isInputActive = activeElement && (
+      activeElement.tagName === 'INPUT' || 
+      activeElement.tagName === 'TEXTAREA' || 
+      (activeElement as HTMLElement).isContentEditable
+    );
+
+    if (event.key === '/' && !isInputActive) {
+      event.preventDefault();
+      if (this.searchbar) {
+        this.searchbar.setFocus();
+      }
+    }
   }
 
   private getCompetitionData() {
@@ -58,3 +79,4 @@ export class CompetitionsPage extends CapacitorBase implements OnInit {
     }, 2000);
   };
 }
+
